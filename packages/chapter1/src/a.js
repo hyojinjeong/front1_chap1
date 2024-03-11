c9db86f013863a4b3705830e96bd9cc543d97acd
hyojinjeong <hyojin.dev@gmail.com>
2024-03-12 07:14:40 +0900

    add : solution a-1

--- packages/chapter1/src/a.js
+++ packages/chapter1/src/a.js
@@ -18,12 +18,13 @@
  */
 
 // 생성자 함수는 수정하지 마세요
-function Worker (health) {
+function Worker(health) {
   this._health = health ?? 10;
 }
 
 function JuniorEngineer(health, intelligence) {
-  this._super(health);
+  // this._super(health);
+  Worker.call(this, health);
   this._intelligence = intelligence ?? 1;
   if (this._intelligence > 10) {
     this._isBornGenius = true;
@@ -32,13 +33,30 @@ function JuniorEngineer(health, intelligence) {
 //- 생성자 함수는 수정하지 마세요
 
 // 여기에 코드를 작성하세요
-// TO-DO
+Worker.prototype.getHealth = function () {
+  return this._health;
+}
+Worker.prototype.work = function () {
+  return this._health--;
+}
+
+JuniorEngineer.prototype = Object.create(Worker.prototype);
+JuniorEngineer.prototype.constructor = JuniorEngineer;
+
+JuniorEngineer.prototype.getIntelligence = function () {
+  return this._intelligence;
+}
+
+JuniorEngineer.prototype.work = function () {
+  Worker.prototype.work.call(this);
+  this._intelligence++;
+}
+
+JuniorEngineer.prototype.isBornGenius = function () {
+  return this._isBornGenius ?? false;
+}
+
 //- 여기에 코드를 작성하세요
-
-
-
-
-
 
 /**
  * ## 문제 A - 추가문제
@@ -69,7 +87,7 @@ function JuniorEngineer(health, intelligence) {
 //     new JuniorEngineer(10, Math.floor(Math.random() * 20)).isBornGenius();
 //   }
 //   var endTime = performance.now();
-  
+
 //   console.log(endTime - startTime);
 // }
 
