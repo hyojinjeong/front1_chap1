255e5f9f7633deb59c2a3d810ab96235a85d68a9
hyojinjeong <hyojin.dev@gmail.com>
2024-03-28 20:38:10 +0900

    add: 과제 E

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,27 @@
+let currentCallback = null;
+
 export const 구독 = fn => {
+  currentCallback = fn;
   fn();
+  currentCallback = null;
 }
 
 export const 발행기관 = obj => {
+  Object.keys(obj).forEach(key => {
+    let val = obj[key];
+    const observers = new Set();
+
+    Object.defineProperty(obj, key, {
+      get() {
+        if (currentCallback) observers.add(currentCallback);
+        return val;
+      },
+
+      set(value) {
+        val = value;
+        observers.forEach(fn => fn());
+      }
+    })
+  })
   return obj;
 }
